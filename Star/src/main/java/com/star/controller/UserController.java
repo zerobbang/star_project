761b2fe9b6dab4f763cb40e9b02f5962cdf60406
WoobinHwang <dnqls0326@naver.com>
2022-08-02 15:05:31 +0900

    1. gradle에 의존성 주입 2. properties에 mail api 설정 추가 3. 메일 계정을 입력하면 0~9까지의 랜덤 난수 발송

--- Star/src/main/java/com/star/controller/UserController.java
+++ Star/src/main/java/com/star/controller/UserController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.star.domain.MailDto;
 import com.star.service.UserService;
 
 @Controller
@@ -17,4 +19,21 @@ public class UserController {
 	public String openUser(Model model) {
 		return "star/main";
 	}
+	
+	public UserController(UserService userService) {
+        this.userService = userService;
+    }
+	
+	@GetMapping(value = "/star/sendmail.do")
+	public String openMailPage(Model model) {
+		return "star/sendmail";
+	}
+ 
+    @PostMapping("/mail/send")
+    public String sendMail(MailDto mailDto) {
+        userService.sendSimpleMessage(mailDto);
+        System.out.println("메일 전송 완료");
+        return "star/sendmail";
+    }
+	
 }
--- Star/src/main/java/com/star/service/UserService.java
+++ Star/src/main/java/com/star/service/UserService.java
@@ -1,7 +1,6 @@
 package com.star.service;
 
-import java.util.List;
-
+import com.star.domain.MailDto;
 import com.star.domain.UserDTO;
 
 public interface UserService {
@@ -9,4 +8,7 @@ public interface UserService {
 	public boolean registerUser(UserDTO params);
 	
 	public UserDTO getUser(Long userNumber);
+	
+	public void sendSimpleMessage(MailDto mailDto);
+	
 }
