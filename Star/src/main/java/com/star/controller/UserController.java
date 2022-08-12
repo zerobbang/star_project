e64f7a3ce03b812f3e23ff70213a94cfcf2b0180
kmk3593 <alsrbs0219@gmail.com>
2022-08-12 12:51:36 +0900

    메인페이지 중간 테이블 rttr -> model

--- Star/src/main/java/com/star/controller/UserController.java
+++ Star/src/main/java/com/star/controller/UserController.java
@@ -39,19 +39,36 @@ public class UserController {
 	}
 	
 	// 메인페이지 ( with. 중간에 있는 테이블 )
+//	@GetMapping(value = "/star/main3")
+//	public String openPredictionList(DustDTO params, RedirectAttributes rttr) {
+//		List<DustDTO> dustList = userService.getPrediction(params);
+//		rttr.addFlashAttribute(dustList);
+//
+//		System.out.println(params);
+//		System.out.println(params.getRegion());
+//		rttr.getFlashAttributes();
+//		
+//		System.out.println(dustList.get(0).getHumidity());
+//		System.out.println(dustList.get(1).getHumidity());
+//		return "star/main3";
+//		
+//	}
+	
+	
+	// 메인페이지 ( with. 중간에 있는 테이블 )
 	@GetMapping(value = "/star/main3")
-	public String openPredictionList(DustDTO params, RedirectAttributes rttr) {
+	public String openPredictionList(DustDTO params, Model model) {
 		List<DustDTO> dustList = userService.getPrediction(params);
-		rttr.addFlashAttribute(dustList);
-
+		model.addAttribute("dustList", dustList); 
+		System.out.println("dd");
+		System.out.println(model);
 		System.out.println(params);
-		System.out.println(params.getRegion());
-		rttr.getFlashAttributes();
-		
+		System.out.println(params.getRegion()); 
+		model.addAttribute("selectRegion", params.getRegion());
+		 
 		System.out.println(dustList.get(0).getHumidity());
 		System.out.println(dustList.get(1).getHumidity());
-		return "star/main3";
-		
+		return "star/main3"; 
 	}
 
 	@GetMapping(value = "/star/sendmail.do")
