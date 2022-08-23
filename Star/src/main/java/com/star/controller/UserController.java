42e13b2924261daf5f0851daea49655d0f1961e9
ZeroBBang <gydiane@naver.com>
2022-08-24 07:00:03 +0900

    예보 값 불러오기 - 미완..

--- Star/src/main/java/com/star/controller/UserController.java
+++ Star/src/main/java/com/star/controller/UserController.java
@@ -30,6 +30,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.mysql.cj.x.protobuf.MysqlxDatatypes.Array;
 import com.star.domain.DustDTO;
 import com.star.domain.MailDTO;
 import com.star.domain.UserDTO;
@@ -67,6 +68,7 @@ public class UserController {
 	// ajax 중간 main - map
 	@GetMapping(value = "/star/main2")
 	public String openMap(Model model) throws IOException {
+		
 		// 서울 메타데이타
 		Map<String,List<Integer>> weather = new HashMap<>();
 		ArrayList<Integer> junglanggu = new ArrayList<Integer>(Arrays.asList(62,128));
@@ -85,138 +87,196 @@ public class UserController {
 		ArrayList<Integer> gulogu = new ArrayList<Integer>(Arrays.asList(58,125));
 		
 		weather.put("종로구", jonglogu);
-		weather.put("중구", jonglogu);
-		weather.put("용산구", yongsangu);
-		weather.put("성동구", seongdonggu);
-		weather.put("광진구", gwangjingu);
-		weather.put("동대문구", dongdaemongu);
-		weather.put("중랑구", junglanggu);
-		weather.put("성북구", seongdonggu);
-		weather.put("강북구", dongdaemongu);
-		weather.put("도봉구", dobonggu);
-		weather.put("노원구", dobonggu);
-		weather.put("은평구", mapogu);
-		weather.put("서대문구", mapogu);
-		weather.put("마포구", mapogu);
-		weather.put("양천구", gangseogu);
-		weather.put("강서구", gangseogu);
-		weather.put("구로구", gulogu);
-		weather.put("금천구", geumcheongu);
-		weather.put("영등포구", gangseogu);
-		weather.put("동작구", dongjakgu);
-		weather.put("관악구", dongjakgu);
-		weather.put("서초구", seochogu);
-		weather.put("강남구", gangnamgu);
-		weather.put("송파구", gwangjingu);
-		weather.put("강동구", gwangjingu);
+//		weather.put("중구", jonglogu);
+//		weather.put("용산구", yongsangu);
+//		weather.put("성동구", seongdonggu);
+//		weather.put("광진구", gwangjingu);
+//		weather.put("동대문구", dongdaemongu);
+//		weather.put("중랑구", junglanggu);
+//		weather.put("성북구", seongdonggu);
+//		weather.put("강북구", dongdaemongu);
+//		weather.put("도봉구", dobonggu);
+//		weather.put("노원구", dobonggu);
+//		weather.put("은평구", mapogu);
+//		weather.put("서대문구", mapogu);
+//		weather.put("마포구", mapogu);
+//		weather.put("양천구", gangseogu);
+//		weather.put("강서구", gangseogu);
+//		weather.put("구로구", gulogu);
+//		weather.put("금천구", geumcheongu);
+//		weather.put("영등포구", gangseogu);
+//		weather.put("동작구", dongjakgu);
+//		weather.put("관악구", dongjakgu);
+//		weather.put("서초구", seochogu);
+//		weather.put("강남구", gangnamgu);
+//		weather.put("송파구", gwangjingu);
+//		weather.put("강동구", gwangjingu);
 		
 		// 현재 날짜
 		LocalDate now = LocalDate.now();
+		String formatedNow = "";
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
-		String formatedNow = now.format(formatter);
-		System.out.println(formatedNow);
+//		String formatedNow = now.format(formatter);
+//		System.out.println(formatedNow);
 		
 		// 현재 시간
 		LocalTime nowTime = LocalTime.now();
-		int hour = nowTime.getHour();
-		int min = nowTime.getMinute();
+		String formatedNowTime = "";
 		
 		// 단기 예보 (SKY 값 존재 , 기준 시간 부터 72시간 값 )
 		// - Base_time : 0200, 0500, 0800, 1100, 1400, 1700, 2000, 2300 (1일 8회)
 		// - API 제공 시간(~이후) : 02:10, 05:10, 08:10, 11:10, 14:10, 17:10, 20:10, 23:10
 		
-		ArrayList<Integer> baseTime = new ArrayList<>(Arrays.asList(2,5,8,11,14,17,20,23));
-		String formatedNowTime;
-
-		System.out.println(baseTime);
+		ArrayList<Integer> baseTime = new ArrayList<>(Arrays.asList(2,5,8,11,14,17,20,23));	
 		
-//		if(min>10 && baseTime.contains(hour)) {
-//			formatedNowTime = 
-//		}
-		
-		
-		
-		System.out.println(hour-1);
-		if(hour-1>=10) {
-			formatedNowTime = (hour-1)+"00";
-			System.out.println(formatedNowTime);
-		}else {
-			formatedNowTime = "0"+(hour-1)+"00";
-			System.out.println(formatedNowTime);
+		// 시간 비교하기
+		for(int i : baseTime) {
+			LocalTime criTime = LocalTime.of(i, 10, 30);
+			if(nowTime.isAfter(criTime) == true) {
+				// DateTimeFormatter formatterTime = DateTimeFormatter.ofPattern("HHmm");
+				// String formatedNowTime = nowTime.format(formatterTime);
+				if(i<10) {
+					formatedNow = now.format(formatter);
+					formatedNowTime = "0"+i+"00";
+					break;
+				}else {
+					formatedNow = now.format(formatter);
+					formatedNowTime = i+"00";
+					break;
+				}					
+			}else {
+				// 23시 이후인 경우
+				formatedNow = now.minusDays(1).format(formatter);
+				formatedNowTime = "2300";
+				break;
+			}
 		}
 		
+		// 람다식을 사용할 때 로컬 변수를 사용하려면 final 특성이어야 한다. (불가변성)  > 그래서 재 선언
+		String formatedNowFinal = formatedNow;
+		String formatedNowTimeFinal = formatedNowTime;
+//		System.out.println(formatedNowFinal);
+//		System.out.println(formatedNowTimeFinal);
+			
 		// 메인 페이지로 넘겨줄 list data
-		Map<String,Float> mapWeather = new HashMap<>();
+		Map<String,List> mapWeather = new HashMap<>();
 		
-//		weather.forEach((key,value)->{
-//	        try {
-//
-//				int x =value.get(0);
-//				int y = value.get(1);
-//
-//				// 현재 대기 정보 open API
-//				StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"); /*URL*/
-//				urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=2OLJexa7ycFuyZomTegF0Z1G5ar6arjzMfEcvT%2B3ewmnNYnDdOfXG0FJFy3%2BzA%2F7spRHr%2BRTraC%2Fnxzak73dFg%3D%3D");
-//				urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
-//		        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("1000", "UTF-8")); /*한 페이지 결과 수*/
-//		        urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*요청자료형식(XML/JSON) Default: XML*/
-//		        urlBuilder.append("&" + URLEncoder.encode("base_date","UTF-8") + "=" + URLEncoder.encode("20220823", "UTF-8")); 
-//		        urlBuilder.append("&" + URLEncoder.encode("base_time","UTF-8") + "=" + URLEncoder.encode("0500", "UTF-8"));      
-//	        	urlBuilder.append("&" + URLEncoder.encode("nx","UTF-8") + "=" + URLEncoder.encode(x+"", "UTF-8")); /*예보지점의 X 좌표값*/
-//		        urlBuilder.append("&" + URLEncoder.encode("ny","UTF-8") + "=" + URLEncoder.encode(y+"", "UTF-8")); /*예보지점의 Y 좌표값*/
-//		        
-//		        
-//		        // URL 객체 생성
-//		        URL url = new URL(urlBuilder.toString());
-//		        // 연결
-//		        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//		        conn.setRequestMethod("GET");
-//		        conn.setRequestProperty("Content-type", "application/json");
-//		        System.out.println("Response code: " + conn.getResponseCode());
-//		        
-//		        BufferedReader rd;
-//		        // 서버 연결 코드에 상관없이 rd로 데이터를 받아온다.
-//		        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-//		            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-//		        } else {
-//		            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-//		        }
-//		        
-//		        // StringBuilder로 생성
-//		        StringBuilder sb = new StringBuilder();
-//		        String line;
-//		        // 서버로부터 받아온 데이터 rd를 line으로 변수명 지정
-//		        // 널값이 아니면 stringbuilder에 데이터 추가
-//		        while ((line = rd.readLine()) != null) {
-//		            sb.append(line);
-//		        }
-//		        // 연결 종료
-//		        rd.close();
-//		        conn.disconnect();
-//		        
-//		        String data = sb.toString();
-//		        // 1. 문자열 형태의 JSON을 파싱하기 위한 JSONParser 객체 생성.
-//		        JSONParser parser = new JSONParser();
-//		        // 2. 문자열을 JSON 형태로 JSONObject 객체에 저장. 	
-//		        try {
-//					JSONObject obj = (JSONObject)parser.parse(data);
-//					System.out.println(obj);
-//					JSONObject response = (JSONObject) obj.get("response");
-//					JSONObject body = (JSONObject) response.get("body");
-//					JSONObject items = (JSONObject) body.get("items");
-//					JSONArray item = (JSONArray) items.get("item");
+		weather.forEach((key,value)->{
+	        try {
+				int x =value.get(0);
+				int y = value.get(1);
+
+				// 현재 대기 정보 open API
+				StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"); /*URL*/
+				urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=2OLJexa7ycFuyZomTegF0Z1G5ar6arjzMfEcvT%2B3ewmnNYnDdOfXG0FJFy3%2BzA%2F7spRHr%2BRTraC%2Fnxzak73dFg%3D%3D");
+				urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
+		        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("1000", "UTF-8")); /*한 페이지 결과 수*/
+		        urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*요청자료형식(XML/JSON) Default: XML*/
+		        urlBuilder.append("&" + URLEncoder.encode("base_date","UTF-8") + "=" + URLEncoder.encode(formatedNowFinal, "UTF-8")); 
+		        urlBuilder.append("&" + URLEncoder.encode("base_time","UTF-8") + "=" + URLEncoder.encode(formatedNowTimeFinal, "UTF-8"));      
+	        	urlBuilder.append("&" + URLEncoder.encode("nx","UTF-8") + "=" + URLEncoder.encode(x+"", "UTF-8")); /*예보지점의 X 좌표값*/
+		        urlBuilder.append("&" + URLEncoder.encode("ny","UTF-8") + "=" + URLEncoder.encode(y+"", "UTF-8")); /*예보지점의 Y 좌표값*/
+		        
+		        
+		        // URL 객체 생성
+		        URL url = new URL(urlBuilder.toString());
+		        // 연결
+		        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		        conn.setRequestMethod("GET");
+		        conn.setRequestProperty("Content-type", "application/json");
+		        System.out.println("Response code: " + conn.getResponseCode());
+		        
+		        BufferedReader rd;
+		        // 서버 연결 코드에 상관없이 rd로 데이터를 받아온다.
+		        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+		            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		        } else {
+		            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+		        }
+		        
+		        // StringBuilder로 생성
+		        StringBuilder sb = new StringBuilder();
+		        String line;
+		        // 서버로부터 받아온 데이터 rd를 line으로 변수명 지정
+		        // 널값이 아니면 stringbuilder에 데이터 추가
+		        while ((line = rd.readLine()) != null) {
+		            sb.append(line);
+		        }
+		        // 연결 종료
+		        rd.close();
+		        conn.disconnect();
+		        
+		        String data = sb.toString();
+		        // 1. 문자열 형태의 JSON을 파싱하기 위한 JSONParser 객체 생성.
+		        JSONParser parser = new JSONParser();
+		        // 2. 문자열을 JSON 형태로 JSONObject 객체에 저장. 	
+		        try {
+					JSONObject obj = (JSONObject)parser.parse(data);
+					JSONObject response = (JSONObject) obj.get("response");
+					JSONObject body = (JSONObject) response.get("body");
+					JSONObject items = (JSONObject) body.get("items");
+					JSONArray item = (JSONArray) items.get("item");
+					// System.out.println(item.size());
+					Map<String,Float> air = new HashMap<>();
+					ArrayList<Map> airList = new ArrayList<>();
+					for(int i=0;i<item.size();i++) {
+						System.out.println(i);
+						
+						JSONObject itemList = (JSONObject) item.get(i);
+						String category = (String) itemList.get("category");
+						if(category.equals("PTY") || category.equals("SKY") || category.equals("TMP")) {
+							System.out.println(category);
+							// System.out.println(String.valueOf(itemList.get("fcstValue")));
+							float airVal = Float.parseFloat(String.valueOf(itemList.get("fcstValue")));
+							// System.out.println("airVal"+airVal);
+							System.out.println(itemList);
+							air.put(category,airVal);
+							System.out.println("-"+air);
+
+							
+						}
+						// 예보시간
+						// air.put("fcstTime", (Float) itemList.get("fcstTime"));
+						// air.put("fcstDate", (Float) itemList.get("fcstDate"));
+//						airList.add(air);
+//						System.out.println(air.size());
+//						System.out.println("아");
+//						if(air.size()==3) {
+//							System.out.println("한시간 지남");
+//							
+//							air.clear();
+//						}
+						if(air.size()==3) {
+							System.out.println("한시간 지남");
+							System.out.println(air);
+							airList.add(air);
+							System.out.println(airList);
+							air.clear();
+						}
+						
+						
+						
+					}
+					
+					
+					System.out.println("-----"+airList);
+					mapWeather.put(key, airList);
+					System.out.println(mapWeather);
+					
+					// System.out.println(cate);
 //					System.out.println(item);
-////					JSONObject tempData = (JSONObject) item.get(3);
-////					float temp = Float.parseFloat(String.valueOf(tempData.get("obsrValue")));
-////					mapWeather.put(key, temp);
-//				} catch (ParseException e) {
-//					e.printStackTrace();
-//				}
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			} 
-//	       
-//		});
+//					JSONObject tempData = (JSONObject) item.get(3);
+//					float temp = Float.parseFloat(String.valueOf(tempData.get("obsrValue")));
+//					mapWeather.put(key, temp);
+					
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} 
+	       
+		});
         
 		model.addAttribute("mapWeather", mapWeather);
 		
